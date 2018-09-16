1ab0edce5108807cb41326ac4a11b73d5c91ef02
Dacian Tamasan <project.java.marvel@gmail.com>
2018-09-16 21:08:08 +0200

    Initial commit

---
+++ src/main/java/com/marvel/marvelheroes/MarvelHeroesApplication.java
@@ -0,0 +1,12 @@
+package com.marvel.marvelheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelHeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelHeroesApplication.class, args);
+	}
+}
